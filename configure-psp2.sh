0473b497692af83c7d0f9832da2a96ed63afe3c2
David <cpasjuste@gmail.com>
2017-02-07 13:23:46 +0100

    Update configure-psp2.sh

--- configure-psp2.sh
+++ configure-psp2.sh
@@ -10,7 +10,7 @@ sed -i 's/CFLAGS="$CFLAGS -Werror=implicit-function-declaration"//g' configure.p
 #sed -i 's/CFLAGS="$CFLAGS -Werror=missing-prototypes"//g' configure.psp2
 sed -i 's/-DHAVE_LIBDRM/-DNOTHAVE_LIBDRM/g' configure.psp2
 
-CFLAGS="-Wl,-q -D__PSP2__ -O2 -mfpu=neon -mcpu=cortex-a9 -march=armv7-a -mfloat-abi=hard -ffast-math -DM_PI_2=1.57079632679489661923 -DM_PI_4=0.78539816339744830962" \
+CFLAGS="-Wl,-q -D__PSP2__ -O3 -mfpu=neon -mcpu=cortex-a9 -march=armv7-a -mfloat-abi=hard -ffast-math -DM_PI_2=1.57079632679489661923 -DM_PI_4=0.78539816339744830962" \
 CXXFLAGS="$CFLAGS -fpermissive" \
 ./configure.psp2 --host=arm-vita-eabi --prefix=$VITASDK/arm-vita-eabi --disable-shared --enable-static \
 	--disable-xvmc \
